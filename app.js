963810195649f4d5b57c072b94263a6764e7f206
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-01 15:39:37 -0700

    completes HTML and adds instructions in js.

---
+++ app.js
@@ -0,0 +1,15 @@
+//Get DOM elements
+
+//Add event listener for user choice
+
+//Add event listner for play button
+
+//Generate a random throw for the computer
+
+//How to pick a winner in a given throw(game)
+
+//Display result of game
+
+//Update wins, losses, draws
+
+//STRETCH to add event listner to restart game
