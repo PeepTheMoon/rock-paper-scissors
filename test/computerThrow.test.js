6bd8ed88338f35e8a672c697c9488f5aebadbc38
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-02 11:28:52 -0700

    another updatet to tests

--- test/computerThrow.test.js
+++ test/computerThrow.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
-import { computerThrow } from '../computerThrow.js';
+//import { computerThrow } from '../computerThrow.js';
 
 // import example from '../src/example.js';
 
-const test = QUnit.test;
+//const test = QUnit.test;
 
-test('should return rock, paper, or scissors', function(assert) {
+//test('should return rock, paper, or scissors', function(assert) {
     //Arrange
     // Set up your parameters and expectations
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const rock = computerThrow(1);
+    //const rock = computerThrow(1);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(rock, 'rock');
-});
+    //assert.equal(rock, 'rock');
+//});
